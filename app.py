b979ed95c4cbc5a72528d5c6b72fba78e1702715
Anmol <gujjranmol257@gmail.com>
2026-05-06 19:22:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,6 @@ if uploaded_file is not None:
 
                     img = image.resize((150, 150))
                     img_array = tf.keras.preprocessing.image.img_to_array(img)
-                    img_array = img_array / 255.0
                     img_array = np.expand_dims(img_array, axis=0)
 
                     prediction = model.predict(img_array, verbose=0)[0][0]
